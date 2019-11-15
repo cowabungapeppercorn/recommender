ec0b4a4fa56c843f2dcd0fa01518b5d92eb8b15a
Nick <nst447@gmail.com>
2019-11-14 16:50:57 -0800

    Added userContext to app and wrapped everything in provider

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { decode } from 'jsonwebtoken';
+
+import BackendApi from '../backendApi';
 import Routes from './Routes/Routes';
 import NavBar from './NavBar/NavBar';
-import { decode } from 'jsonwebtoken';
+import { UserContext } from '../userContext';
+
 import './App.css';
-import BackendApi from '../backendApi';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -19,19 +22,21 @@ function App() {
     }
   }
 
-  useEffect(function() {
+  useEffect(function () {
     try {
       getCurrentUser();
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
   }, []);
 
   return (
     <div className="App">
-      <NavBar />
-      <h1>{currentUser}</h1>
-      <Routes />
+      <UserContext.Provider value={currentUser}>
+        <NavBar />
+        <h1>{currentUser.username}</h1>
+        <Routes />
+      </UserContext.Provider>
     </div>
   );
 }
