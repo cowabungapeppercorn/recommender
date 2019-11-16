6e0b526449c1381544be52adade8a6dcf89bfbc8
Nick <nst447@gmail.com>
2019-11-16 11:17:00 -0800

    Added logout navLink

--- frontend/src/App/NavBar/NavBar.js
+++ frontend/src/App/NavBar/NavBar.js
@@ -3,7 +3,19 @@ import { NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import { UserContext } from '../../userContext';
 
-function NavBar() {
+function NavBar(props) {
+  const loginNavLink = (
+    <NavLink to="/register" className="mx-3">
+      {'login'}
+    </NavLink>
+  );
+
+  const logoutNavLink = (
+    <NavLink to="/" className="mx-3" onClick={props.handleLogout}>
+      {'logout'}
+    </NavLink>
+  )
+
   return (
     <UserContext.Consumer>
       {currentUser => (
@@ -24,9 +36,7 @@ function NavBar() {
             <NavLink to="/artists" className="mx-3">
               {'artists'}
             </NavLink>
-            <NavLink to="/register" className="mx-3">
-              {currentUser ? 'logout' : 'login'}
-            </NavLink>
+            {currentUser ? logoutNavLink : loginNavLink}
           </Nav>
         </Navbar>
       )}
