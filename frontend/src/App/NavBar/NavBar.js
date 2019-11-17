3c1d6450989a2e9e046d4cacc00e08daa2151a7d
Nick <nst447@gmail.com>
2019-11-17 14:05:36 -0800

    Refactored navbar

--- frontend/src/App/NavBar/NavBar.js
+++ frontend/src/App/NavBar/NavBar.js
@@ -1,46 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import { UserContext } from '../../userContext';
 
 function NavBar(props) {
+  const { currentUser, handleLogout } = useContext(UserContext);
+  
   const loginNavLink = (
-    <NavLink to="/register" className="mx-3">
+    <NavLink to="/auth" className="mx-3">
       {'login'}
     </NavLink>
   );
 
   const logoutNavLink = (
-    <NavLink to="/" className="mx-3" onClick={props.handleLogout}>
+    <NavLink to="/" className="mx-3" onClick={handleLogout}>
       {'logout'}
     </NavLink>
-  )
+  );
 
   return (
-    <UserContext.Consumer>
-      {currentUser => (
-        <Navbar bg="dark" variant="dark" className="justify-content-between">
-          <Navbar.Brand className="ml-3">
-            <NavLink to="/">
-              {'recommender'}
-            </NavLink>
-          </Navbar.Brand>
+    <Navbar bg="dark" variant="dark" className="justify-content-between">
+      <Navbar.Brand className="ml-3">
+        <NavLink to="/">
+          {'recommender'}
+        </NavLink>
+      </Navbar.Brand>
 
-          <Nav className="mr-3">
-            <NavLink to="/songs" className="mx-3">
-              {'songs'}
-            </NavLink>
-            <NavLink to="/albums" className="mx-3">
-              {'albums'}
-            </NavLink>
-            <NavLink to="/artists" className="mx-3">
-              {'artists'}
-            </NavLink>
-            {currentUser ? logoutNavLink : loginNavLink}
-          </Nav>
-        </Navbar>
-      )}
-    </UserContext.Consumer>
+      <Nav className="mr-3">
+        <NavLink to="/songs" className="mx-3">
+          {'songs'}
+        </NavLink>
+        <NavLink to="/albums" className="mx-3">
+          {'albums'}
+        </NavLink>
+        <NavLink to="/artists" className="mx-3">
+          {'artists'}
+        </NavLink>
+        {currentUser ? logoutNavLink : loginNavLink}
+      </Nav>
+    </Navbar>
   )
 }
 
