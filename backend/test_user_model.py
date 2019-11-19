73708e9a85bbcec948fcac345353c45d2d8d28db
Nick <nst447@gmail.com>
2019-11-19 13:53:40 -0800

    Added some method tests for user model

--- backend/test_user_model.py
+++ backend/test_user_model.py
@@ -10,13 +10,13 @@ from config import TEST_DATABASE_NAME
 from app import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql:///{TEST_DATABASE_NAME}'
+app.config['SQLALCHEMY_ECHO'] = False
 
-db.drop_all()
-db.create_all()
 
 class UserModelTestCase(TestCase):
 
     def setUp(self):
+        db.create_all()
         User.query.delete()
 
         user_1 = User.register("slam", "slam")
@@ -31,6 +31,7 @@ class UserModelTestCase(TestCase):
 
     def tearDown(self):
         db.session.rollback()
+        db.drop_all()
 
     def test_user_model(self):
         users = User.query.all()
@@ -48,3 +49,31 @@ class UserModelTestCase(TestCase):
         self.assertIn(u1, users)
         self.assertIn(u2, users)
         self.assertEqual(len(users), 2)
+
+    def test_serialize(self):
+        u1 = User.query.filter_by(username=self.user_1.username).first()
+        u2 = User.query.filter_by(username=self.user_2.username).first()
+
+        u1_serialized = {
+            "username": "slam",
+            "id": 1
+        }
+
+        u2_serialized = {
+            "username": "cowabunga",
+            "id": 2
+        }
+
+        self.assertEqual(u1.serialize(), u1_serialized)
+        self.assertEqual(u2.serialize(), u2_serialized)
+
+    def test_get_by_username(self):
+        u1 = User.get_by_username("slam")
+        u2 = User.get_by_username("cowabunga")
+
+        self.assertEqual(u1.username, "slam")
+        self.assertEqual(u2.username, "cowabunga")
+        self.assertEqual(u1.id, 1)
+        self.assertEqual(u2.id, 2)
+        self.assertIsInstance(u1, User)
+        self.assertIsInstance(u2, User)
